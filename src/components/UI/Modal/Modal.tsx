b9b4d127de2cb11f7c2f3f84ce2c755f9ca8fc6c
ulyana <novikova.ula.007@gmail.com>
2026-02-07 09:33:37 +0300

    add UI components [modal, spinner], fix NavBar

---
+++ src/components/UI/Modal/Modal.tsx
@@ -0,0 +1,38 @@
+import {Dialog, DialogContent, DialogTitle, IconButton} from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
+import * as React from "react";
+
+interface Props extends React.PropsWithChildren{
+    show: boolean;
+    title?: string;
+    onClose?: () => void;
+}
+
+
+const Modal: React.FC<Props> = ({show, title, onClose, children}) => {
+    return (
+        <Dialog open={show} onClose={onClose} fullWidth maxWidth="xs">
+            <DialogTitle>
+                {title}
+                {(
+                    <IconButton
+                        aria-label="close"
+                        onClick={onClose}
+                        sx={{
+                            position: 'absolute',
+                            right: 8,
+                            top: 10,
+                        }}
+                    >
+                        <CloseIcon />
+                    </IconButton>
+                )}
+            </DialogTitle>
+            <DialogContent dividers>
+                {children}
+            </DialogContent>
+        </Dialog>
+    );
+};
+
+export default Modal;
