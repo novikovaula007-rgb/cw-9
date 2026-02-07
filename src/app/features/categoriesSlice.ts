2caebcea0a0d9b5bb7716db354e1b01047b586bf
ulyana <novikova.ula.007@gmail.com>
2026-02-07 09:13:08 +0300

    add store, category slice

---
+++ src/app/features/categoriesSlice.ts
@@ -0,0 +1,78 @@
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import type {ICategory, ICategoryAPI} from "../../types";
+import type {AppDispatch, RootState} from "../store/store.ts";
+import {axiosAPI} from "../../axiosAPI.ts";
+
+interface categoriesState {
+    categories: ICategory[],
+    loading: {
+        deleteCategory: string | null,
+        fetchAllCategories: boolean
+    }
+}
+
+const initialState: categoriesState = {
+    categories: [],
+    loading: {
+        deleteCategory: null,
+        fetchAllCategories: false
+    }
+}
+
+export const fetchAllCategories = createAsyncThunk<ICategory[]>(
+    'categories/fetchAllCategories',
+    async () => {
+        const response = await axiosAPI.get<ICategoryAPI | null>('categories.json');
+        const categoriesObject = response.data;
+
+        if (!categoriesObject) {
+            return []
+        } else {
+            return Object.keys(categoriesObject).map(key => ({
+                ...categoriesObject[key],
+                id: key,
+            }));
+        }
+    }
+)
+
+export const deleteCategoryById = createAsyncThunk<void, string, {dispatch: AppDispatch}>(
+    'categories/deleteCategoryById',
+    async (id, thunkAPI) => {
+        await axiosAPI.delete(`/categories/${id}.json`);
+        await thunkAPI.dispatch(fetchAllCategories());
+    }
+);
+
+const categoriesSlice = createSlice({
+    name: 'categories',
+    initialState,
+    reducers: {},
+    extraReducers: builder => {
+        builder.addCase(fetchAllCategories.pending, (state) => {
+            state.loading.fetchAllCategories = true;
+        });
+        builder.addCase(fetchAllCategories.fulfilled, (state, {payload: items}) => {
+            state.loading.fetchAllCategories = false;
+            state.categories = items;
+        });
+        builder.addCase(fetchAllCategories.rejected, (state) => {
+            state.loading.fetchAllCategories = false;
+        });
+
+        builder.addCase(deleteCategoryById.pending, (state, {meta}) => {
+            state.loading.deleteCategory = meta.arg;
+        });
+        builder.addCase(deleteCategoryById.fulfilled, (state) => {
+            state.loading.deleteCategory = null;
+        });
+        builder.addCase(deleteCategoryById.rejected, (state) => {
+            state.loading.deleteCategory = null;
+        });
+    }
+})
+
+export const selectAllCategories = (state: RootState) => state.categories.categories;
+export const selectCategoriesLoading = (state: RootState) => state.categories.loading;
+
+export const categoriesReducer = categoriesSlice.reducer;
